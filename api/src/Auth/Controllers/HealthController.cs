f11139be7cbba2c8e385d6b7f6a34e79343714c0
Stephen Lomangino <iceman371@yahoo.com>
2022-03-19 23:20:58 -0400

    add health controler and deployment files for auth

---
+++ api/src/Auth/Controllers/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Auth.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Auth.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HealthController : Controller
+    {
+        private readonly ILogger<HealthController> _logger;
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            _logger.LogInformation("Health!");
+            return Ok("Healthy");
+        }
+    }
+}
