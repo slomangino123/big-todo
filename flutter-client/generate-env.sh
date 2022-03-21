8277c34a3b8eaff4c195d0139cb4c0d531d9d85a
Stephen Lomangino <iceman371@yahoo.com>
2022-03-20 23:43:01 -0400

    update flutter build to use 'flutter-' prefix to git tag and added simple environment variable generation script

--- flutter-client/generate-env.sh
+++ flutter-client/generate-env.sh
@@ -1,5 +1,29 @@
-cat << EOF > .env
-API=http://10.0.0.102:7000
-AUTH=10.0.0.102:5000
-ENV=Development
+#!/bin/bash -e
+
+ENV_REGEX="^[d,p]$"
+
+if [[ -z $1 || ! $1 =~ $ENV_REGEX ]]; then
+    echo "First parameter must specify environment: d|p"
+    exit 1
+fi
+
+API=""
+AUTH=""
+ENV=""
+
+if [[ $1 == 'd' ]]; then
+    API="http://big-todo-api-d.eba-tvmneqax.us-east-1.elasticbeanstalk.com"
+    AUTH="http://big-todo-auth-d.eba-vwxrnf4w.us-east-1.elasticbeanstalk.com"
+    ENV="Development"
+fi
+
+if [[ -z $API || -z $AUTH || -z $ENV ]]; then
+    echo "One of the variables wasnt set!"
+    exit 1
+fi
+
+cat <<EOF > .env
+API=$API
+AUTH=$AUTH
+ENV=$ENV
 EOF
